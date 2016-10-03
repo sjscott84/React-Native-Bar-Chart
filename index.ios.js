526687d591e162825a931a6d9a0f97c49da71eca
Sarah Scott <s.j.scott84@gmail.com>
2016-10-02 17:16:42 -0700

    Add comments and change heigt

--- index.ios.js
+++ index.ios.js
@@ -25,8 +25,8 @@ import Svg,{
     Stop
 } from 'react-native-svg';
 
-var dataset = [5, 6.7, 4.3, 8, 11, 5, 3, 2.6, 8];
-var width = Dimensions.get('window');
+var dataset = [5, 6.7, 4.3, 8, 11, 5, 3, 2.6, 8, 10, 14, 4.3, 8, 11, 5, 3, 2.6, 8, 10, 14];
+var screenWidth = Dimensions.get('window');
 
 class testGraph extends Component {
   render() {
@@ -42,21 +42,19 @@ class testGraph extends Component {
   _svgGraph(){
     let rectangles = []; //Array of bars
     let text = []; //tick points on axis
-    let tickLines = []; //tick lines
-
-    console.log(width.width);
+    let tickLines = []; //tick lines;
 
     //Measurments for placement of axis
     const lowNumberForXAxis = 32;
     const lowNumberForYAxis = 0;
-    const highNumberForYAxis = 299;
-    const highNumberForXAxis = width.width-20;//This will need to be based on the screen width of device
-    const barPadding = 1;
+    const highNumberForYAxis = 115;
+    const highNumberForXAxis = screenWidth.width-20;//Screen width minus the margins
+    const barPadding = 2;
 
     //Get max number from data set
     let max = function(){
       let currentMax = dataset[0];
-      for(var i = 0; i<dataset.length; i++){
+      for(var i = 0; i < dataset.length; i++){
         if(dataset[i] > currentMax){
           currentMax = dataset[i];
         }
@@ -67,31 +65,35 @@ class testGraph extends Component {
     getTicks(3);
     getBars();
 
+    //Get the relative height based on the propportion between max number and total screen height
     function getHeight(axisHeight, datasetMax, newNumber){
-      let height = (newNumber/datasetMax)*axisHeight;
+      let height = (newNumber / datasetMax) * axisHeight;
       return height;
     };
 
+    //Get the axis labels and lines
     function getTicks(numberofTicks){
-      let fontSize = 11;
       let lineHeight = 14;
-      text.push(<Text key={0} stroke="black" fontSize={fontSize} x={1} y={highNumberForYAxis-12}>{'0'}</Text>);
-      text.push(<Text key={max} stroke="black" fontSize={fontSize} x={1} y={lowNumberForYAxis}>{max}</Text>);
-      tickLines.push(<Line key={max} x1={lowNumberForXAxis} y1={lowNumberForYAxis+1} x2={highNumberForXAxis} y2={lowNumberForYAxis+1} stroke='gray' strokeWidth={0.75} />);
+      //Create the axis labels for top and bottom as well as the top axis line (these are done seperatly to ensure they display correctly)
+      text.push(<Text key={0} style={styles.text} x={1} y={highNumberForYAxis - lineHeight}>{'0'}</Text>);
+      text.push(<Text key={max} style={styles.text} x={1} y={lowNumberForYAxis}>{max}</Text>);
+      tickLines.push(<Line key={max} x1={lowNumberForXAxis} y1={lowNumberForYAxis + 1} x2={highNumberForXAxis} y2={lowNumberForYAxis + 1} stroke='gray' strokeWidth={0.75} />);
 
+      //Create the axis labels and lines for everthing in between 0 and max, numbers displayed to 2 decimal places
       for(var i = 1; i < numberofTicks; i++){
-        let tickText = Math.round(((max/numberofTicks)*[i])*100)/100;
+        let tickText = Math.round(((max / numberofTicks) * [i]) * 100) / 100;
         let tickY = getHeight(highNumberForYAxis, max, tickText);
-        text.push(<Text key={tickText} stroke="black" lineHeight={lineHeight} fontSize={fontSize} x={1} y={highNumberForYAxis-tickY-(lineHeight/2)}>{tickText}</Text>);
-        tickLines.push(<Line key={tickText} x1={lowNumberForXAxis} y1={highNumberForYAxis-tickY} x2={highNumberForXAxis} y2={highNumberForYAxis-tickY} stroke='gray' strokeWidth={0.75} />);
+        text.push(<Text key={tickText} style={styles.text} lineHeight={lineHeight} x={1} y={highNumberForYAxis - tickY - (lineHeight / 2)}>{tickText}</Text>);
+        tickLines.push(<Line key={tickText} x1={lowNumberForXAxis} y1={highNumberForYAxis - tickY} x2={highNumberForXAxis} y2={highNumberForYAxis - tickY} stroke='gray' strokeWidth={0.75} />);
       }
     };
 
+    //Create the bars for the graph
     function getBars(){
       for(var i = 0; i < dataset.length; i++){
         let height = getHeight(highNumberForYAxis, max, dataset[i]);
-        let width = (highNumberForXAxis - lowNumberForXAxis) / dataset.length - 4;
-        let x = (lowNumberForXAxis + barPadding) + rectangles.length * (highNumberForXAxis - lowNumberForXAxis) / dataset.length;//rectangles.length * highNumberForXAxis / dataset.length
+        let width = (highNumberForXAxis - lowNumberForXAxis) / dataset.length - barPadding;
+        let x = (lowNumberForXAxis + barPadding) + rectangles.length * (highNumberForXAxis - lowNumberForXAxis - 5) / dataset.length;
         let y = highNumberForYAxis - height;
         rectangles.push(<Rect key={rectangles.length} x={x} y={y} width={width} height={height} fill={'rgb(23,153,173)'} />)
       }
@@ -121,8 +123,11 @@ const styles = StyleSheet.create({
     marginRight: 10,
     flex: 1,
     height: 300
-    //justifyContent: 'center',
-    //alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'AppleSDGothicNeo-Medium',
+    fontSize: 10,
+    color: 'rgb(122,143,147)'
   }
 });
 
